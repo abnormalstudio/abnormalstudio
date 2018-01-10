7a0138551d39643b424d25fef080dab66649dea5
Leigh Halliday <leighhalliday@gmail.com>
2018-01-10 16:39:57 -0500

    Setting up elements folder and creating main element

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import Nav from '../components/Nav';
+import React from "react";
+import Nav from "../components/Nav";
+import { Main } from "../elements/main";
 
 export default ({ children }) => (
   <div style={{ margin: `0 auto` }}>
     <h1>Abnormal Studio</h1>
-
-    <main>
-      <section
-        style={{
-          margin: `0 auto`,
-          width: `75vw`,
-          height: `60vh`,
-          border: `1px solid #212121`,
-          background: `#212121`,
-          color: `#FFFFFF`
-        }}
-      >
-        {children()}
-      </section>
-      <Nav />
-    </main>
+    <Main>{children()}</Main>
+    <Nav />
   </div>
 );
