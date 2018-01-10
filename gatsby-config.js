a5430b9015b2a9141cd5dd4938146a1e4d09adab
Marian Serna <marianhalliday@gmail.com>
2018-01-10 08:40:21 -0500

    Adding required plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,22 @@
 module.exports = {
-  plugins: [`gatsby-plugin-styled-components`]
+  siteMetadata: {
+    siteUrl: `https://www.abmornalstudio.com`,
+    title: `ABnormalStudio`
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-accessibilityjs`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `limelight`,
+          `source sans pro\:300,400,400i,700` // you can also specify font weights and styles
+        ]
+      }
+    }
+  ]
 };
