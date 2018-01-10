86d4cf9e9d5ce4095491f3cbd0d3a24a39e15146
Leigh Halliday <leighhalliday@gmail.com>
2018-01-10 16:26:08 -0500

    Creating About page place holder

---
+++ src/pages/about.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+export default class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <Helmet>
+          <title>About Abnormal Studio</title>
+        </Helmet>
+      </div>
+    );
+  }
+}
