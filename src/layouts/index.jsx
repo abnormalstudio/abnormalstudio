9b875373778b56da8bddc5160c50b38fb25e3d3b
Marian Serna <marianhalliday@gmail.com>
2018-01-10 16:41:27 -0500

    Extracting nav from main

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -5,20 +5,19 @@ export default ({ children }) => (
   <div style={{ margin: `0 auto` }}>
     <h1>Abnormal Studio</h1>
 
-    <main>
-      <section
-        style={{
-          margin: `0 auto`,
-          width: `75vw`,
-          height: `60vh`,
-          border: `1px solid #212121`,
-          background: `#212121`,
-          color: `#FFFFFF`
-        }}
-      >
-        {children()}
-      </section>
-      <Nav />
+    <main
+      style={{
+        margin: `0 auto`,
+        width: `75vw`,
+        height: `60vh`,
+        border: `1px solid #212121`,
+        background: `#212121`,
+        color: `#FFFFFF`
+      }}
+    >
+      {children()}
     </main>
+
+    <Nav />
   </div>
 );
