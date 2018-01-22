378c4b8d56b95b8c19ae209d05a1aeb41d0f1905
Leigh Halliday <leighhalliday@gmail.com>
2018-01-22 13:51:05 -0500

    Changing social share buttons

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -1,36 +1,41 @@
 import React from "react";
-import { FacebookButton, LinkedInButton, TwitterButton } from "react-social";
 import SimpleLineIcon from "react-simple-line-icons";
 import { SocialIcons } from "../elements/main";
 import { colors } from "../elements/settings";
 
+import { ShareButtons } from "react-share";
+const {
+  FacebookShareButton,
+  LinkedinShareButton,
+  TwitterShareButton
+} = ShareButtons;
+
 export default () => (
   <SocialIcons>
-    <FacebookButton
+    <FacebookShareButton
       aria-label="Facebook"
       className="btn btn-social"
-      appId="144478576347781"
-      sharer={true}
+      url={"https://www.abnormalstudio.com"}
     >
       <SimpleLineIcon
         name="social-facebook"
         size="Large"
         color={colors.greyish}
       />
-    </FacebookButton>
-    <TwitterButton aria-label="Twitter" className="btn btn-social">
+    </FacebookShareButton>
+    <TwitterShareButton aria-label="Twitter" className="btn btn-social">
       <SimpleLineIcon
         name="social-twitter"
         size="Large"
         color={colors.greyish}
       />
-    </TwitterButton>
-    <LinkedInButton aria-label="LinkedIn" className="btn btn-social">
+    </TwitterShareButton>
+    <LinkedinShareButton aria-label="LinkedIn" className="btn btn-social">
       <SimpleLineIcon
         name="social-linkedin"
         size="Large"
         color={colors.greyish}
       />
-    </LinkedInButton>
+    </LinkedinShareButton>
   </SocialIcons>
 );
