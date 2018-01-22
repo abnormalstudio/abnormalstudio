4b92c917443bec94e7b31e57212046b2a028c5c4
Leigh Halliday <leighhalliday@gmail.com>
2018-01-21 19:09:56 -0500

    Setting meta tags

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Motion, spring } from "react-motion";
+import { Helmet } from "react-helmet";
 import Nav from "../components/Nav";
 import Logo from "../components/Logo";
 import SocialIcons from "../components/SocialIcons";
@@ -7,6 +8,14 @@ import "../styles/reset.css";
 import "../styles/common.scss";
 import { Abnormal, Main } from "../elements/main";
 import { colors } from "../elements/settings";
+
+const siteName = "ABNORMAL studio";
+const keywords =
+  "agency, studio, web development, javascript, react, ruby, ruby on rails, shopify, threejs, web animation";
+const description =
+  "ABNORMAL studio is a small agency dedicated to making creative ideas accessible for everyone. We love challenges, no project is too small for us!";
+const siteUrl = "https://www.abnormalstudio.com";
+const imageUrl = "https://s3.amazonaws.com/abnormalstudio/abnormalstudio.jpg";
 
 export default ({ children }) => (
   <div
@@ -16,6 +25,32 @@ export default ({ children }) => (
       background: `${colors.white}`
     }}
   >
+    <Helmet>
+      <html lang="en" />
+      <title>{siteName}</title>
+
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="author" content={siteName} />
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+      <meta name="title" content={siteName} />
+
+      <meta property="og:title" content={siteName} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={siteUrl} />
+      <meta property="og:image" content={imageUrl} />
+      <meta property="og:site_name" content={siteName} />
+      <meta property="og:description" content={description} />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content={siteUrl} />
+      <meta name="twitter:creator" content="@abnormalstudio" />
+      <meta name="twitter:title" content={siteName} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={imageUrl} />
+    </Helmet>
+
     <Logo />
     <Motion
       defaultStyle={{ opacity: 0 }}
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
-import { Contactcontent, Para, Linking } from '../elements/content';
+import React from "react";
+import { Helmet } from "react-helmet";
+import { Contactcontent, Para, Linking } from "../elements/content";
 
 export default class Contact extends React.Component {
   render() {
     return (
       <div>
         <Helmet>
-          <title>Contact Abnormal Studio</title>
+          <title>Contact | ABNORMAL studio</title>
+          <meta property="og:title" content="Contact | ABNORMAL studio" />
+          <meta name="twitter:title" content="Contact | ABNORMAL studio" />
         </Helmet>
 
         <Contactcontent>
           <Para>Would you like to get in touch?</Para>
 
           <Para>
-            Drop us a line at{' '}
+            Drop us a line at{" "}
             <Linking
               href="mailto:studio@abnormal.com.com"
               target="_blank"
